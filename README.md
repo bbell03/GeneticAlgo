8905119e42d62ef26d9b6aa9bea307c3b8ace2bd
bbell03 <bbell03@tufts.edu>
2019-11-24 15:36:12 -0500

    all done :)

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# GeneticAlgo
+###### COMP 131: Artificial Intelligence
+###### Homework 4: Genetic Algorithms -- Knapsack Problem
+
+### Code Details
+
+### IMPLEMENTATION
+  This is our implementation of the knapsack problem using a Genetic Algorithm
+  as described by the specification on canvas.
+
+  * The knapsack problem simulates the placement of items in a knapsack.
+  * Each knapsack item has the following properties:
+    * a weight attribute value,
+    * and an importance attribute value.
+
+###ARCHITECTURE
+  This implementation uses genetic algorithms as an optimization to return the ideal combination of items in the backpack with
+  the total weight not exceeding the maximum capacity of 120, and the total
+  value of items in the backpack being as large as possible.
+
+#### FUNCTION(S) IMPLEMENTED INCLUDE:
+
+### METHODOLOGY
---
+++ gene.py
@@ -0,0 +1,109 @@
+###### COMP 131: Artificial Intelligence
+###### Homework 4: Genetic Algorithms -- Knapsack Problem
+
+import random
+
+def crossover(parent1, parent2):
+    length_seg = random.randint(1,6)
+    child1 = []
+    child2 = []
+
+    for k in range(length_seg):
+        child1.append(parent1[k])
+        child2.append(parent2[k])
+
+    for l in range(7 - length_seg):
+        child1.append(parent2[l+length_seg])
+        child2.append(parent1[l+length_seg])
+
+    return child1, child2
+
+def generator():
+    genome = []
+    for i in range(7):
+        genome.append(random.randint(0,1))
+    return genome
+
+def sort_list(tup_list):
+    return sorted(tup_list, key = lambda x: x[0], reverse = True)
+
+class Knapsack():
+    def __init__(self):
+        self.dimen = 7
+        self.maxweight = 120
+        #will hold genomes
+        self.population = []
+        self.fit = []
+        self.weight = [20, 30, 60, 90, 50, 70, 30]
+        self.importance = [6, 5, 8, 7, 6, 9, 4]
+
+    def fitness(self, genome):
+        sum_weight = 0
+        sum_importance = 0
+
+        for i in range(self.dimen):
+            sum_weight += genome[i]*self.weight[i]
+            sum_importance += genome[i]*self.importance[i]
+
+        if sum_weight > self.maxweight:
+            return (sum_importance - 200)
+        else:
+            return sum_importance
+
+    def pool(self):
+
+        if(len(self.population) == 0):
+            for i in range(100):
+                self.population.append(generator())
+                self.fit.append(self.fitness(self.population[i]))
+        else:
+            for i in range(100):
+                self.fit.append(self.fitness(self.population[i]))
+
+        fittest = list(zip(self.fit,self.population))
+        fittest = sort_list(fittest)
+
+        for i in range(50):
+            self.population[i] = fittest[i][1]
+
+        for k in range(0, 49, 2):
+            tuple = crossover(fittest[k][1], fittest[k+1][1])
+
+            self.population[50+k] = tuple[0]
+            self.population[50+k+1] = tuple[1]
+
+        for l in range(50,100):
+            self.population[l] = self.mutation(self.population[l])
+
+        self.fit = []
+        for i in range(100):
+            self.fit.append(self.fitness(self.population[i]))
+        fittest = list(zip(self.fit,self.population))
+        fittest = sort_list(fittest)
+
+        self.fit = []
+
+    def mutation(self, genome):
+
+        mutated = genome
+
+        if(random.randint(1,100) < 10):
+            i = random.randint(0,6)
+
+            if (genome[i] == 0):
+                mutated[i] = 1
+            else:
+                mutated[i] = 0
+
+        if (self.fitness(genome) > self.fitness(mutated)):
+            return genome
+        else:
+            return mutated
+
+
+###MAIN
+bag = Knapsack()
+for i in range(15):
+    bag.pool()
+print(bag.population[0])
+print(bag.population[99])
